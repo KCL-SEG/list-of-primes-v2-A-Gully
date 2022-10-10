906766a141d431d8f6f6bcaf18867abfb21fc26f
A-Gully <agallul@yahoo.com>
2022-10-10 13:08:59 +0100

    Done

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Inappropriate amount")
     list = []
+    i = 0
+    num = 2
+    num2 = num//2
+    while i in range(number_of_primes):
+        for num2 in range(2, num):
+            if (num % num2) == 0:
+                num += 1
+                break
+        else:
+            list.insert(i, num)
+            i += 1
+            num += 1
+    print(list)
     return list
+
+
+print(27//2)
+primes(10)
